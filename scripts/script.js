3e536947ca7eadc3c12d14430e0c96b930487a71
Raian-Siddique <raian0197@gmail.com>
2020-08-17 22:35:39 +0600

    version-2

--- scripts/script.js
+++ scripts/script.js
@@ -62,7 +62,7 @@ function displaySearchResult(songs) {
   }
 }
 
-// remove lyrics
+// go back to search result
 function hide() {
   const displayArea = document.getElementById("lyricsArea");
   displayArea.remove();
